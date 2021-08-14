7876c0f5d446281939a3d6af0bc93211624229b3
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2021-08-14 15:41:09 +0900

    Puma confg

--- config/puma.rb
+++ config/puma.rb
@@ -14,7 +14,7 @@ unless ENV['RAILS_ENV'] == 'development'
   shared_dir = "#{app_dir}/shared"
 
   # Default to production
-  rails_env = ENV['RAILS_ENV'] || "production"
+  rails_env = "production"
   environment rails_env
 
   # Set up socket location
@@ -33,6 +33,11 @@ unless ENV['RAILS_ENV'] == 'development'
     ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
     ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
   end
+else
+  # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+  #
+  port ENV.fetch("PORT") { 3000 }
+  environment ENV.fetch("RAILS_ENV") { "development" }
 end
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
@@ -40,16 +45,8 @@ end
 #
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port ENV.fetch("PORT") { 3000 }
-
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
-
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+pidfile ENV.fetch("PIDFILE") { "shared/pids/server.pid" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
